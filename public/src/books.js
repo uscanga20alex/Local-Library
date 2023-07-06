311b5b04581159b7db0f3fe52dd3ffae3143172a
Alejandro uscanga <126415540+uscanga20alex@users.noreply.github.com>
2023-07-05 20:05:10 -0400

    Update books.js
    adding more comment's to functions.

--- public/src/books.js
+++ public/src/books.js
@@ -1,17 +1,21 @@
+//look up an author by it's id
 function findAuthorById(authors, id) {
   return authors.find((author) => author.id === id);
 }
 
+//match book id to id
 function findBookById(books, id) {
   return books.find((book) => book.id === id);
 }
 
+//filter out what books are and are not barrowed into two different groups.
 function partitionBooksByBorrowedStatus(books) {
   const borrowedBooks = books.filter((book) => book.borrows[0].returned === false);
   const returnedBooks = books.filter((book) => book.borrows[0].returned === true);
   return [borrowedBooks, returnedBooks]
 }
 
+//create a new array for each book that's borrowed by id.
 function getBorrowersForBook(book, accounts) {
   const borrowers = book.borrows.map((borrow) => {
     const account = accounts.find((acc) => acc.id === borrow.id);
